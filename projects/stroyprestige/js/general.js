dd843313271151f29111b59355e9b724ca2ef920
a.kashevich@sam-solutions.com <a.kashevich@sam-solutions.com>
2016-07-11 16:28:09 +0300

    stroyprestige

--- projects/stroyprestige/js/general.js
+++ projects/stroyprestige/js/general.js
@@ -6,14 +6,38 @@
 		var selectors = {
 			sliderFront: '.slider-front__inner-wrapper',
 			servicesFront: '.services-front__inner-wrapper',
-			projectsFront: '.slider-projects__inner-wrapper'
+			projectsFront: '.slider-projects__inner-wrapper',
+			clientsFront: '.slider-clients__inner-wrapper',
+			toTopBtnCls: '.js-to-top',
+			expandMapFrontCls: '.js-expand-map'
 		};
 
 		var $global = {
 			sliderFront: $(selectors.sliderFront),
 			servicesFront: $(selectors.servicesFront),
-			projectsFront: $(selectors.projectsFront)
+			projectsFront: $(selectors.projectsFront),
+			clientsFront: $(selectors.clientsFront)
 		};
+
+		$body.on('click', selectors.toTopBtnCls, toTop);
+		$body.on('click', selectors.expandMapFrontCls, expandMapFront);
+
+		function expandMapFront() {
+			var $map = $('#map-front');
+			var posPage = $(document).scrollTop();
+			$map.toggleClass('expand');
+			$map.is('expand') ? scrollPage(posPage - 300, 260) : scrollPage(posPage + 300, 260);
+		}
+
+		function toTop() {
+			scrollPage(0, 520);
+		}
+
+		function scrollPage(value, speed) {
+			$('html, body').animate({
+				scrollTop: value
+			}, speed);
+		}
 
 		(function sliderFrontInit() {
 			var $slider = $global.sliderFront;
@@ -45,6 +69,19 @@
 
 		(function projectsFrontInit() {
 			var $slider = $global.projectsFront;
+			var controls = getControls($slider);
+
+			$slider.slick({
+				arrows: true,
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				prevArrow: controls.prev,
+				nextArrow: controls.next
+			});
+		})();
+
+		(function clientsFrontInit() {
+			var $slider = $global.clientsFront;
 			var controls = getControls($slider);
 
 			$slider.slick({
